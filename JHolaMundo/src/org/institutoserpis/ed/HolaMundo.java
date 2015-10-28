bf2c5c18bc41f86b0e6d0249564967d4692ea165
susanajimenez15 <dam.sjimenez@institutoserpis.org>
2015-10-28 08:50:30 +0100

    añadido array test

--- JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -1,11 +1,13 @@
 package org.institutoserpis.ed;
+import java.util.Scanner;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hola Mundo desde eclipse");
+		Scanner scanner = new Scanner (System.in);
+		String nombre = scanner.nextLine();
+		System.out.println("Hola %s desde eclipse"+nombre);
 		
 	}
 
---
+++ Lenguaje c/ProgNombre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main () {
+
+    char nom[20];
+
+    printf("Dime tu nombre : ");
+    scanf("%s", nom);
+
+    printf("Tu nombre es : %s ",nom);
+
+
+}
